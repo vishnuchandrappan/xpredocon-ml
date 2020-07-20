936b4b682776179d98dd332569fb8b8802c4b0e6
vishnuchandrappan <cvishnu70@gmail.com>
2020-07-20 13:31:46 +0530

    final

--- clean_code.py
+++ clean_code.py
@@ -211,7 +211,7 @@ Name = StringVar()
 w2 = Label(root, justify=LEFT, text="Disease Predictor using Machine Learning", fg="white", bg="blue")
 w2.config(font=("Elephant", 30))
 w2.grid(row=1, column=0, columnspan=2, padx=100)
-w2 = Label(root, justify=LEFT, text="A Project by Yaswanth Sai Palaghat", fg="white", bg="blue")
+w2 = Label(root, justify=LEFT, text="", fg="white", bg="blue")
 w2.config(font=("Aharoni", 30))
 w2.grid(row=2, column=0, columnspan=2, padx=100)
 
